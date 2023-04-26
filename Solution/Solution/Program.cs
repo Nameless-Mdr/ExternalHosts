cefcab41374852997247a337aa492a0211db44c8
radmir <radmirgk@mail.ru>
2023-04-26 15:44:10 +0300

    work algorithm

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,14 @@
+﻿var A = new string[]
+{
+    "unlock.microvirus.md", "visitwar.com", "visitwar.de", "fruonline.co.uk", "australia.open.com", "credit.card.us",
+    "a.b.c.d.e", "x.e.y", "ijkm", "z.p.o", "r.s.t", "f.g.h.l.m"
+};
+var B = new string[]
+    { "microvirus.md", "visitwar.de", "piratebay.co.uk", "list.stolen.credit.card.us", "e", "p.o", "r.s.t", "m" };
+var result = Solution.Solution.solution(A, B);
+
+foreach(var ind in result)
+    Console.Write(ind + " ");
+Console.WriteLine();
+
+Console.ReadKey(true);    
